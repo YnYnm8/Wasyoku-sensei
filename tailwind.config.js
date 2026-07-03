8980ae4bd1a297bb8e84828bdcc9b391cae99b27
YnYnm8 <meikotolouse0726.com>
2026-07-03 16:24:32 +0200

    WIP: Add login/register design (background pattern, title)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        washoku: {
+          bg: '#0B0908',
+          input: '#1F1A18',
+          inputtext: '#8D847E',
+          red: '#8C2121',
+        },
+      },
+      fontFamily: {
+        display: ['"Shippori Mincho"', 'serif'],
+        body: ['"Inter"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
