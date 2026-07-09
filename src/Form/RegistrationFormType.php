24f5e81432a839c2b353001086bb2f43043d7b9c
YnYnm8 <meikotolouse0726.com>
2026-07-09 11:00:43 +0200

    Complete US 4.1/4.2: add username field, email/password confirmation, fix redirects after login/logout/register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,13 +11,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', RepeatedType::class, [
+                'type' => EmailType::class,
+                'first_options' => ['label' => 'Email'],
+                'second_options' => ['label' => 'Confirmer l\'email'],
+            ])
+
+            ->add('username')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,22 +34,18 @@ class RegistrationFormType extends AbstractType
                     ),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank(
-                        message: 'Please enter a password',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        max: 4096,
-                    ),
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'constraints' => [
+                        new NotBlank(message: 'Please enter a password'),
+                        new Length(min: 6, minMessage: 'Your password should be at least {{ limit }} characters', max: 4096),
+                    ],
                 ],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
         ;
     }
