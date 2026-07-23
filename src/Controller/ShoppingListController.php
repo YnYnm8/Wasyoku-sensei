8add20e0edbb8d02514327ed1941c53c22a7928f
YnYnm8 <meikotolouse0726.com>
2026-07-23 15:18:44 +0200

    feat(shopping-list): affichage détaillé des ingrédients/condiments sélectionnés avec nom, quantité et unité (US6.2 CA1)

--- src/Controller/ShoppingListController.php
+++ src/Controller/ShoppingListController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\FavoriteRepository;
+use App\Repository\RecipeCondimentRepository;
+use App\Repository\RecipeIngredientRepository;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,29 +42,74 @@ final class ShoppingListController extends AbstractController
         // （include_recipe_1, include_recipe_23 のように、レシピIDごとに名前が違うので、
         //   $request->request->all() で、送られてきた"全部"を、一旦受け取る）
         $allData = $request->request->all();
+        // ''これはメモがなかったらからの配列として送ってくださいといういみ
         $memo = $request->request->get('memo', '');
+
         // セッションに、今回チェックされた内容・メモを保存する
         $session->set('shopping_confirm_allData', $allData);
-        $session->set('shopping_confirm_memo', $memo);
-        // ''これはメモがなかったらからの配列として送ってくださいといういみ
 
         return $this->redirectToRoute('app_shopping_list_confirm_show');
     }
 
 
     // ③ セッションから、チェック内容・メモを取り出して、確認画面（Image 2相当）を表示する
-    #[Route('/shopping-list/confirm', name: 'app_shopping_list_confirm_show', methods: ['GET'])]
-    #[IsGranted('ROLE_USER')]
-    public function confirmShow(SessionInterface $session): Response
-    {
-        $allData = $session->get('shopping_confirm_allData', []);
-        $memo = $session->get('shopping_confirm_memo', '');
+   #[Route('/shopping-list/confirm', name: 'app_shopping_list_confirm_show', methods: ['GET'])]
+#[IsGranted('ROLE_USER')]
+public function confirmShow(
+    SessionInterface $session,
+    RecipeIngredientRepository $recipeIngredientRepository,
+    RecipeCondimentRepository $recipeCondimentRepository
+): Response {
+   
+    // セッションに保存しておいた、大きな配列（TWIGでINPUTにこのような名前がついているため）を、丸ごと取り出しています。
+    $allData = $session->get('shopping_confirm_allData', []);
+   
+    // 「$allDataという配列の中に、'checked_items'というキーが、もし存在すれば、その値を使う。もし存在しなければ（nullのような扱いになるので）、代わりに、空の配列[]を使う」
+    // この？？があることで、エラーの予防になる
+    $checkedItems = $allData['checked_items'] ?? [];
+    $memo = $allData['memo'] ?? '';
 
-        return $this->render('shopping_list/confirm.html.twig', [
-            'allData' => $allData,
-            'memo' => $memo,
-        ]);
+    $displayItems = [];
+
+    foreach ($checkedItems as $item) {
+        // $itemが、例えば'ingredient_70'だったとします。explode('_', ...)は、「_という記号の場所で、文字列を切り分けて、その結果を、配列として返す」関数
+        $parts = explode('_', $item);
+
+        // $partsという変数の中に、「切り分けられた、2つの文字列が入った、配列」が、入っています。$parts = ['ingredient', '70'];
+        $type = $parts[0];
+
+        // この分けられた配列の中のIDは1番目の順番にあるところだよと教えている
+        $id = $parts[1];
+
+        if ($type === 'ingredient') {
+            $ri = $recipeIngredientRepository->find($id);
+            if ($ri) {
+                $displayItems[] = [
+                    'name' => $ri->getIngredient()->getName(),
+                    'quantity' => $ri->getQuantity(),
+                    'unit' => $ri->getUnit(),
+                ];
+            }
+        } elseif ($type === 'condiment') {
+            $rc = $recipeCondimentRepository->find($id);
+            if ($rc) {
+                $displayItems[] = [
+                    'name' => $rc->getCondiment()->getName(),
+                    'quantity' => $rc->getQuantity(),
+                    'unit' => $rc->getUnit(),
+                ];
+            }
+        }
     }
+
+    return $this->render('shopping_list/confirm.html.twig', [
+        'displayItems' => $displayItems,
+        'memo' => $memo,
+    ]);
+
+}
+
+       
 
     #[Route('/shopping-list/new', name: 'app_shopping_list_new', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
